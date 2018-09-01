a74765a50158531a0d8c99c6f1e39ad9d1d45968
jmitchel3 <jmitchel3@users.noreply.github.com>
2018-09-01 14:59:56 -0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
+import createHistory from 'history/createBrowserHistory'
+
 let API_PUBLIC_KEY = '<your-live-public-key>'
 let API_ENDPOINT = 'https://api.srvup.com/v1'
 
@@ -9,7 +11,10 @@ if (process.env.REACT_APP_DEV_API_ENDPOINT) {
   API_ENDPOINT = process.env.REACT_APP_DEV_API_ENDPOINT
 }
 
+const History = createHistory()
+
 export {
   API_ENDPOINT,
-  API_PUBLIC_KEY
+  API_PUBLIC_KEY,
+  History,
 }
